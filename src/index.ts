6d7eeb54999bc1f6040ac437a03d7620adc8d8d7
thithipl <pleum9112@gmail.com>
2026-05-07 09:19:50 +0700

    v2

--- src/index.ts
+++ src/index.ts
@@ -61,5 +61,5 @@ app.use("/test", (req, res) => {
 });
 
 app.listen(Number(PORT), '0.0.0.0', () => {
-    console.log(`✅ Server is running on http://0.0.0.0:${PORT}`);
+    console.log(`✅ Server is running on http://0.0.0.0:${PORT}`,'V.2');
 });
